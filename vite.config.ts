aea972e0fd72dd020648ef301180cdd751e62ac3
bergaumnn <bergaumnn@gmail.com>
2025-11-04 09:09:39 +0000

    Fix vercel build config

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: "dist", // ✅ тепер збірка буде у client/dist
+    outDir: path.resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
   },
   server: {
